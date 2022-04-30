b1f9f4793cf76ae98dba94a328ae665ca5d2b008
broizter <39500356+broizter@users.noreply.github.com>
2022-04-30 16:39:26 +0200

    Add filename date option and minor fix

--- main.py
+++ main.py
@@ -2,69 +2,79 @@
 
 import requests
 import configparser
+import time
+import os
 
 def authenticate():
-	authUrl = 'https://accounts.spotify.com/api/token'
-	data = {
-		'grant_type': 'refresh_token',
-		'refresh_token': f'{refreshToken}',
-		'Content-Type': 'application/x-www-form-urlencoded',
-		'client_id': f'{clientId}',
-		'client_secret': f'{clientSecret}'
-	}
-	r = (requests.post(authUrl, data=data))
-	token = r.json()['access_token']
-	return token
+        authUrl = 'https://accounts.spotify.com/api/token'
+        data = {
+                'grant_type': 'refresh_token',
+                'refresh_token': f'{refreshToken}',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'client_id': f'{clientId}',
+                'client_secret': f'{clientSecret}'
+        }
+        r = (requests.post(authUrl, data=data))
+        token = r.json()['access_token']
+        return token
 
 def extractTracks(response):
-	tracks = response['items']
-	output = ''
+        tracks = response['items']
+        output = ''
 
-	for t in tracks:
-		unseparatedArtists = []
-		searchArtists = t['track']['artists']
-		for a in searchArtists:
-			unseparatedArtists.append(a['name'])
+        for t in tracks:
+                unseparatedArtists = []
+                searchArtists = t['track']['artists']
+                for a in searchArtists:
+                        unseparatedArtists.append(a['name'])
 
-		mySong = t['track']['name']
-		myAlbum = t['track']['album']['name']
-		myArtists = ', '.join(unseparatedArtists)
+                mySong = t['track']['name']
+                myAlbum = t['track']['album']['name']
+                myArtists = ', '.join(unseparatedArtists)
 
-		output += f'{myArtists} - {myAlbum} - {mySong}\n'
+                output += f'{myArtists} - {myAlbum} - {mySong}\n'
 
-	return output
+        return output
 
-def processPlaylists(playlist, token):
-	headers = {
-		'Authorization': f'Bearer {token}',
-		'Content-Type': 'application/json',
-		'Accept': 'application/json'
-	}
-	baseUrl = f'https://api.spotify.com/v1/playlists/{playlist}/tracks?fields=items(track(name%2Cartists(name)%2Calbum(name))),next'
-	doloop = True
+def processPlaylists(playlist, token, date, datePrefix):
+        headers = {
+                'Authorization': f'Bearer {token}',
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+        }
+        baseUrl = f'https://api.spotify.com/v1/playlists/{playlist}/tracks?fields=items(track(name%2Cartists(name)%2Calbum(name))),next'
+        doloop = True
 
-	result = ''
-	while doloop:
-		r = requests.get(baseUrl, headers=headers) # curl
-		response = r.json() # jq
-		result += extractTracks(response)
+        result = ''
+        while doloop:
+                r = requests.get(baseUrl, headers=headers) # curl
+                response = r.json() # jq
+                result += extractTracks(response)
 
-		if response['next'] is not None:
-			baseUrl = response['next']
-		else:
-			doloop = False
-			with open(f'{outputFolder}/{playlist}.txt', 'w', encoding='utf-8') as f:
-				print(result, end='', file=f)
+                if response['next'] is not None:
+                        baseUrl = response['next']
+                else:
+                        doloop = False
+                        if datePrefix:
+                                with open(f'{outputFolder}/{date}{playlist}.txt', 'w', encoding='utf-8') as f:
+                                        print(result, end='', file=f)
+                        else:
+                                with open(f'{outputFolder}/{playlist}.txt', 'w', encoding='utf-8') as f:
+                                        print(result, end='', file=f)
+
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(os.path.join(os.path.dirname(__file__), 'config.ini'))
 
 playlists = config['main']['playlists'].split(", ")
 outputFolder = config['main']['output_folder']
+datePrefix = config.getboolean('filename', 'date_prefix')
 clientId = config['auth']['client_id']
 clientSecret = config['auth']['client_secret']
 refreshToken = config['auth']['refresh_token']
 
+date = time.strftime("%Y%m%d-")
+
 token = authenticate()
 for p in playlists:
-	processPlaylists(p, token)
+        processPlaylists(p, token, date, datePrefix)
